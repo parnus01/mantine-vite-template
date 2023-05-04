b6439f579eacf0f9a96e6c0f22cf866759f44d0b
Danusorn.t <osacn_danusorn.taboonyong@krungthai.com>
2023-05-04 17:26:03 +0700

    poc clerk authen / add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
+import { ClerkProvider } from '@clerk/clerk-react';
 import { ThemeProvider } from './ThemeProvider';
-import { Welcome } from './Welcome/Welcome';
-import { Container, Text } from '@mantine/core';
+import { AppRoutes } from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
+  const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
   return (
-    <ThemeProvider>
-      <Welcome />
-      <Container>
-        <Text fz="xs">Extra small text</Text>
-        <Text fz="sm">Small text</Text>
-        <Text fz="md">Default text</Text>
-        <Text fz="lg">Large text</Text>
-        <Text fz="xl">Extra large text</Text>
-        <Text fw={500}>Semibold</Text>
-        <Text fw={700}>Bold</Text>
-        <Text fs="italic">Italic</Text>
-        <Text td="underline">Underlined</Text>
-        <Text td="line-through">Strikethrough</Text>
-        <Text c="dimmed">Dimmed text</Text>
-        <Text c="blue">Blue text</Text>
-        <Text c="teal.4">Teal 4 text</Text>
-        <Text tt="uppercase">Uppercase</Text>
-        <Text tt="capitalize">capitalized text</Text>
-        <Text ta="center">Aligned to center</Text>
-        <Text ta="right">Aligned to right</Text>
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <ClerkProvider publishableKey={clerkPubKey}>
+          <AppRoutes />
+        </ClerkProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
